00243ac71b1f8c039ec14f31c0ef66850cc9d54a
MiguelDonado <miguel.5799@hotmail.es>
2024-01-23 17:28:53 +0100

    Complete stretch goals.
    - Add From and To input fields, reverse the order and allow deleting of endorsements.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com
 const endorsementTextAreaEl = document.getElementById("endorsement-textarea")
 const publishBtnEl = document.getElementById("publish-btn")
 const endorsementListEl = document.getElementById("endorsement-list")
+const fromInputEl = document.getElementById("from-input")
+const toInputEl = document.getElementById("to-input")
 
 const appSettings = {databaseURL: "https://we-are-the-champions-70839-default-rtdb.europe-west1.firebasedatabase.app/"}
 const app = initializeApp(appSettings)
@@ -12,7 +14,10 @@ const endorseListInDb = ref(database,"endorseList")
 
 publishBtnEl.addEventListener("click", function (){
     let endorseValue = endorsementTextAreaEl.value
-    push(endorseListInDb,endorseValue)
+    let fromValue = fromInputEl.value
+    let toValue = toInputEl.value
+    const messageObject = {from:fromValue,to:toValue,endorsement:endorseValue}
+    push(endorseListInDb,messageObject)
     clearTextArea()
 })
 
@@ -21,7 +26,7 @@ onValue(endorseListInDb, function(snapshot){
     if (snapshot.exists()){
         let itemsArray = Object.entries(snapshot.val())
         clearEndorsementListEl()
-        for (let i = 0; i < itemsArray.length; i++){
+        for (let i = itemsArray.length - 1; i>=0; i--){
             appendItemToEndorsementListEl(itemsArray[i])
         }
     }else{
@@ -29,30 +34,28 @@ onValue(endorseListInDb, function(snapshot){
     }
 })
 
-
-
-
-
-
-
-
-
-
-
 function clearTextArea(){
     endorsementTextAreaEl.value=null
 }
 
 function clearEndorsementListEl(){
     endorsementListEl.innerHTML=null
+    fromInputEl.value=null
+    toInputEl.value=null
 }
 
 function appendItemToEndorsementListEl(item){
     let itemID = item[0]
-    let itemValue = item[1]
+    let itemValueObject = item[1]
 
     let newEl = document.createElement("li")
-    newEl.textContent = itemValue
+    newEl.innerHTML = newEl.innerHTML = `<span style="display: block;margin-top: 10px;"><strong>To ${itemValueObject.to}</strong></span>
+                                        <br>${itemValueObject.endorsement}
+                                        <br><span style="display: block;margin-top: 10px;"><strong>From ${itemValueObject.from}</strong></span>`
+    newEl.addEventListener("dblclick",function(){
+        let exactLocationOfEndorseinDb = ref(database,`endorseList/${itemID}`)
+        remove(exactLocationOfEndorseinDb)
+    })
     endorsementListEl.append(newEl)
 }
 
